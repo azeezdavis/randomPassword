332d30bddf8e29d0545ca4b458d4551242a525c6
Azeez Davis <azeezdavis18@gmail.com>
2021-01-04 21:47:14 -0500

    new assignment

---
+++ script.js
@@ -0,0 +1,15 @@
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
+let length
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
